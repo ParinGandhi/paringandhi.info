efa71cf0b31b80b6b5462dab03c25b87ea281548
Parin Gandhi <parin.a.gandhi@gmail.com>
2017-10-05 21:11:26 -0400

    Initial commit. Working Hello world coming from the main controller. Angular route configured.

---
+++ app/main/main.controller.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('pg')
+        .controller('MainController', function () {
+
+            var vm = this;
+            vm.testMessage = "Hello world!";
+
+        });
+})();
